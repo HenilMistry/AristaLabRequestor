f06f0f6cb5f3ed53694c93699d1cd7554deaf574
HenilMistry <henilmistry74496@gmail.com>
2024-11-15 10:13:36 +0530

    Refactored code with adding the code of adding and connection:

--- Asset/main.js
+++ Asset/main.js
@@ -9,6 +9,60 @@ const mouse = {
     y: innerHeight/2 
 };
 
+let Nodes, Connections;
+let NodeToolActivated = 0;
+let ports;
+let PORT_ID = 0;
+let click = 0;
+let firstNode = -1;
+let secondNode = null;
+
+let NodeIxia, NodeDut;
+
+// something to be done initially, put here...
+function init() {
+    Nodes = [];
+    Connections = [];
+    ports = [];
+    NodeIxia = [];
+    NodeDut = [];
+    firstNode = null;
+    secondNode = null;
+    PORT_ID = 0;
+    click = 0;
+}
+
+// Animation Loop
+function animationLoop() {
+    requestAnimationFrame(animationLoop);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    Connections.forEach((conn) => {
+        conn.update();
+    });
+
+    Nodes.forEach((node)=>{
+        node.update();
+    });
+
+    if(firstNode!=null) {
+        // console lines are just for testing 
+        // console.log("Drawing ref. line");
+        c.beginPath();
+        c.strokeStyle = "green";
+        c.lineWidth = 5;
+        c.setLineDash([5, 5]);
+        c.moveTo(firstNode.x, firstNode.y);
+        if(secondNode!=null) {
+          c.lineTo(secondNode.x,secondNode.y);
+        } else {
+          c.lineTo(mouse.x,mouse.y);
+        }
+        c.stroke();
+        c.closePath();
+    }
+}
+
 // What to do when window is resized 
 addEventListener("resize", () => {
     canvas.width = innerWidth;
@@ -36,38 +90,28 @@ addEventListener("click",(e) => {
        }
        NodeToolActivated = 0;
     } else if (ActiveTool == Tools.CONNECTION) {
-
+        if(Nodes.length > 0) {
+            Nodes.forEach((node, index) => {
+              if(distance(node.x,node.y,mouse.x,mouse.y) <= node.radius) {
+                // Just for testing...
+                // console.log("Collision with "+index);
+                click++;
+                if(click%2==0) {
+                  // it means it's a second click..
+                  Connections.push(new ConnectionHelper(firstNode, node, c));
+                  console.log(Connections);
+                  click = 0;
+                  firstNode = null;
+                } else {
+                  firstNode = node;
+                }
+              }
+            });
+        } else {
+            alert("No nodes are on the Canvas!");
+        }
     }
-});  
-
-let Nodes, Connections;
-let NodeToolActivated = 0;
-let ports;
-let PORT_ID = 0;
-let click = 0;
-let firstNode = -1;
-let secondNode = null;
-
-let NodeIxia, NodeDut;
-
-// something to be done initially, put here...
-function init() {
-    Nodes = [];
-    Connections = [];
-    ports = [];
-    NodeIxia = [];
-    NodeDut = [];
-}
-
-// Animation Loop
-function animationLoop() {
-    requestAnimationFrame(animationLoop);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-
-    Nodes.forEach((node)=>{
-        node.update();
-    });
-}
+});
 
 init();
 animationLoop();
