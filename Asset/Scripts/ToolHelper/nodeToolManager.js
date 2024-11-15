eea4c77d1244099b04a1dad6ab5273d61b04e5fe
HenilMistry <henilmistry74496@gmail.com>
2024-11-15 09:44:22 +0530

    Added a function to configured nodes

--- Asset/Scripts/ToolHelper/nodeToolManager.js
+++ Asset/Scripts/ToolHelper/nodeToolManager.js
@@ -1,4 +1,17 @@
 const nodeConfigModal = new bootstrap.Modal('#nodeConfigModal');
+const select_nodeType = document.getElementById("nodeType");
+
+const field_nodeAlias = document.getElementById("nodeAlias");
+const field_location = document.getElementById("nodeLocation");
+const chk_enableMoveTo = document.getElementById("enableMoveTo");
+const field_moveTo = document.getElementById("moveTo");
+// select_nodePorts is defined in portConfigManager.js ...
+
+chk_enableMoveTo.addEventListener("click",(e) => {
+    field_moveTo.disabled = !chk_enableMoveTo.checked;
+});
+
+let lastX=0, lastY=0;
 
 function openNodeConfig() {
     nodeConfigModal.show();
@@ -10,7 +23,35 @@ function closeNodeConfig() {
     ActiveTool = Tools.NODE;
 }
 
-const select_nodeType = document.getElementById("nodeType");
+function configureNode() {
+    let newAlias = field_nodeAlias.value;
+    let newLocation = field_location.value;
+
+    // Check if type is ixia and no ports are configured ?...
+    if(select_nodeType.value == "ixia") {
+        if(select_nodePorts.options.length == 0) {
+            alert("Configure at least one ixia port!");
+            return;
+        }
+        // collect the data and store ixia node...
+        let newPorts = ports; // defined in main.js ...
+        let newIxiaNode = new Ixia(newAlias, newLocation, newPorts);
+        NodeIxia.push(newIxiaNode);
+        ports = [];
+
+    } else { // collect the data and store DUT Node...
+        let newDut;
+        if(chk_enableMoveTo.checked) {
+            newDut = new Dut(newAlias, newLocation, field_moveTo.value);
+        } else {
+            newDut = new Dut(newAlias, newLocation, Dut.NO_MOVETO);
+        }
+        NodeDut.push(newDut);
+    }
+
+    Nodes.push(new Node(newAlias, lastX, lastY, 20, "Red", c));
+    closeNodeConfig();
+}
 
 select_nodeType.addEventListener("change",(e) => {
     const collectionIxia = document.getElementsByClassName("ixia");
