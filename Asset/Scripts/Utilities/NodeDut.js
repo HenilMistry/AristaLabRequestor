4cd80bc71e435ec21b4a56276ea7c856494faa75
HenilMistry <henilmistry74496@gmail.com>
2024-12-29 11:08:05 +0530

    Added a unique object idenification function

--- Asset/Scripts/Utilities/NodeDut.js
+++ Asset/Scripts/Utilities/NodeDut.js
@@ -1,9 +1,28 @@
 const NO_MOVETO = "DO NOT MOVE";
 class Dut {
+    // Identification Number...
+    #id = undefined;
+
     constructor(alias, location, movement) {
+        // Assign the random, unique ID to this object...
+        this.#id = this.#generateUniqueID();
+        // Get the values of variables...
         this.alias = alias;
         this.location = location;
         this.movement = movement;
+    }
+
+    getID() {
+        return this.#id;
+    }
+
+    /**
+     * This function will assign the random ID to each and every object of type Dut
+     * 
+     * @returns Randome and Unique ID String...
+     */
+    #generateUniqueID() {
+        return Date.now().toString(36) + Math.random().toString(36).substring(2, 10);
     }
     
     static get NO_MOVETO() {
