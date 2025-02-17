9d0de77fbb8ebb62b6b3059ce35064152eeff7cf
HenilMistry <henilmistry74496@gmail.com>
2025-02-17 16:57:08 +0530

    Added the const variable for adding image with ease as prepend

--- Asset/Scripts/ToolHelper/toolManager.js
+++ Asset/Scripts/ToolHelper/toolManager.js
@@ -11,6 +11,9 @@ var ActiveTool = null;
 // A variable, that is HTML Element, that is contianing information text 
 const label_info = document.getElementById("label_info");
 
+// This is image of information icon, you can use this for prepadding your information label...
+const infoImagePath = '<img src="./Asset/Icons/info.png" alt="Information" width="30px"> ';
+
 // A function to select the toolbar, which will update the ActiveTool avariable
 function selectTool(name) {
     switch(name) {
@@ -19,11 +22,11 @@ function selectTool(name) {
             secondNode = null;
             ActiveTool=Tools.NODE;
             NodeToolActivated = 1;
-            label_info.innerHTML = '<img src="./Asset/Icons/info.png" alt="Information" width="30px"> Selected Node Tool : Now, click anywhere on canvas to add node!';
+            label_info.innerHTML = infoImagePath + 'Selected Node Tool : Now, click anywhere on canvas to add node!';
             break;
         
         case Tools.NODE_SELECTION_STATE:
-            label_info.innerHTML = '<img src="./Asset/Icons/info.png" alt="Information" width="30px"> <b> Selected node : '+selectedNode.label+'! Click again to open node manager. </b>';
+            label_info.innerHTML = infoImagePath + '<b> Selected node : '+selectedNode.label+'! Click again to open node manager. </b>';
             break;
         
         case Tools.CONNECTION:
@@ -32,7 +35,7 @@ function selectTool(name) {
                 selectedNode = null;
             }
             ActiveTool=Tools.CONNECTION;
-            label_info.innerHTML = '<img src="./Asset/Icons/info.png" alt="Information" width="30px"> Selected Connection Tool : Now, click on any two nodes subsequently to add a connection!';
+            label_info.innerHTML = infoImagePath + 'Selected Connection Tool : Now, click on any two nodes subsequently to add a connection!';
             break;
         
         default:
