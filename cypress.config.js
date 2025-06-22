29be91965a69a3f82e8b3ad234688eab3fcf58d0
HenilMistry <henilmistry74496@gmail.com>
2025-06-22 11:50:57 +0530

    Skipped the folders which has unwated spec files to run

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    excludeSpecPattern: [
+      "**/1-getting-started/*.cy.js",
+      "**/2-advanced-examples/*.cy.js"
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
