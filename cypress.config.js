d03deb11b2c807b747e8d10d9a03d413a3cf98ae
HenilMistry <henilmistry74496@gmail.com>
2025-06-22 17:08:27 +0530

    Added screenshot and video configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    screenshotOnRunFailure: true,
+    baseUrl: "http://localhost:8000/",
     excludeSpecPattern: [
       "**/1-getting-started/*.cy.js",
       "**/2-advanced-examples/*.cy.js"
