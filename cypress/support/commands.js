8d1a470857e78af381da2450c4b9df4b4daac9d4
HenilMistry <henilmistry74496@gmail.com>
2025-06-26 23:50:00 +0530

    Added custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +24,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/**
+ * This custom command click on Node Tool button
+ * and verifies that the label is updated.
+ */
+Cypress.Commands.add('clickOnNodeTool', () => {
+    cy.get("button[title='Node Tool']").click().then(() => {
+        cy.get("#label_info").invoke("text").then((txt) => {
+            expect(txt.trim()).to.match(/Selected Node Tool/i)
+        });
+    })
+});
+
+/**
+ * This command clicks on canvas at given point 
+ * (x, y) and verifies whether the NodeConfigura-
+ * tionModal is visible.
+ */
+Cypress.Commands.add('clickOnCanvas', (x, y) => {
+    cy.get("canvas").should("be.visible");
+    cy.get("canvas").dblclick(x, y);
+    cy.get("#nodeConfigModal").should("be.visible");
+})
