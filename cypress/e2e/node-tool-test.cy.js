0d6031c2981de129e0cef33fb7af9922c2fdde94
HenilMistry <henilmistry74496@gmail.com>
2025-06-26 23:49:42 +0530

    Added test for node tool

--- cypress/e2e/node-tool-test.cy.js
+++ cypress/e2e/node-tool-test.cy.js
@@ -6,16 +6,10 @@ describe("Test cases for testing of Node Tool", () => {
 
    it("Checks whether the Node Configuration Modal is working", () => {
         // 1) Click on "Node Tool" button and verifies that the label text is updated correctly
-        cy.get("button[title='Node Tool']").click().then(() => {
-            cy.get("#label_info").invoke("text").then((txt) => {
-                expect(txt.trim()).to.match(/Selected Node Tool/i)
-            });
-        })
+        cy.clickOnNodeTool();
 
         // 2) Click on some point on canvas and verify the Node Configuration Modal
-        cy.get("canvas").should("be.visible");
-        cy.get("canvas").dblclick(200, 300);
-        cy.get("#nodeConfigModal").should("be.visible");
+        cy.clickOnCanvas(200, 300);
         cy.get("#nodeType").invoke("val").then((selectedValue) => {
             expect(selectedValue).to.match(/dut/i);
         });
@@ -25,6 +19,27 @@ describe("Test cases for testing of Node Tool", () => {
         cy.get("#nodeConfigModal > div > div > div.modal-header > button").click().then(() => {
             cy.get("#nodeConfigModal").should("not.be.visible");
         })
+   })
+
+   it("Checks whether the Node Type Dropdown is working", () => {
+        // 1) Click on "Node Tool" button and verifies the label
+        cy.clickOnNodeTool();
+
+        // 2) Verifies the functionality of Node Type Dropdown / Select
+        cy.clickOnCanvas(100, 200);
+        cy.get("#nodeType").select("IXIA");
+        cy.get("#moveTo").should("not.be.visible");
+        cy.get("#nodeType").select("DUT");
+        cy.get("#moveTo").should("be.visible");
+
+        // 3) Click on close button to close Node Configuration Modal
+        cy.get("#nodeConfigModal > div > div > div.modal-header > button").click().then(() => {
+            cy.get("#nodeConfigModal").should("not.be.visible");
+        })
+   })
+
+   it("Checks whether the Node Configuration is working", () => {
+    
    })
 
 });
