c241308594c9f9e2d601ef95a838c08a5b95a4da
HenilMistry <henilmistry74496@gmail.com>
2025-06-29 00:18:15 +0530

    Added global variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    TOOL_NODE: "Node Tool",
+    TOOL_CONNECTION: "Connection Tool",
+    TOOL_OUTPUT: "Output Tool",
+    TOOL_SETTINGS: "Settings Tool",
+    TOOL_MANUAL: "User Manual",
+    TOOL_ISSUE: "Report an issue",
+    TEXT_LBL_INFO: "#label_info"
+  },
   e2e: {
     video: true,
     screenshotsFolder: 'cypress/screenshots',
